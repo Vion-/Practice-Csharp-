4ff49478d7ef1a23dbd2fa1663d520dd44e33e67
Vion- <bryan19929@msn.com>
2014-12-05 12:28:56 -0600

    Adding Code

---
+++ A+B app/ConsoleApplication3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numberOne = 0;
+            double numberTwo = 0;
+            //Used to see if user entered a correct #
+            
+
+            Console.WriteLine("Sum Application \n---------------\n");
+            verifyInput("Provide Number 1", ref numberOne);
+            verifyInput("Provide Number 2", ref numberTwo);
+
+            Console.WriteLine("Result: " + (numberOne+numberTwo));
+            Console.ReadLine();
+        }
+
+        private static void verifyInput (string message, ref double number)
+        {
+            string input;
+            double value;
+            bool accepted = false;
+            while (accepted == false)
+            {
+                Console.WriteLine(message);
+                input = Console.ReadLine();
+                if(double.TryParse(input, out value))
+                {
+                    accepted = true;
+                    number = value;
+                }
+            }
+        }
+    }
+}
